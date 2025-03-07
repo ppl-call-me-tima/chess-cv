d34107eb63d23925e60968da2cfb528568c4099c
Amit Upadhyay <2648amitupadhyay@gmail.com>
2025-03-07 16:42:58 +0530

    Add a blank screen with indication that corners are not being detectec and final chessboard cannot be generated

--- helpers/board_helpers.py
+++ helpers/board_helpers.py
@@ -88,3 +88,24 @@ def generate_board(board: np.ndarray[np.float32], detections: sv.Detections, key
         cv2.imshow("board", board)
     
     return board_pieces_xy
+
+
+def draw_blank():
+    text = "Corners\nNot\nDetected"
+    font = cv2.FONT_HERSHEY_COMPLEX
+    scale = 2
+    thickness = 2
+    color = (255, 255, 255)
+    
+    text_size, _ = cv2.getTextSize(text, font, scale, thickness)
+    
+    x, y0 = (0, 50)
+    line_height = text_size[1] + 5
+    
+    img = np.zeros((350, 350), np.uint8)
+    
+    for i, line in enumerate(text.split("\n")):
+        y = y0 + i * line_height
+        cv2.putText(img, line, (x, y), font, scale, color, thickness)
+    
+    return img
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 from ultralytics import YOLO
 
 from helpers.detection_helpers import piece_detections, corner_keypoints
-from helpers.board_helpers import draw_board, generate_board
+from helpers.board_helpers import draw_board, generate_board, draw_blank
 from helpers.chessboard_helpers import Chessboard
 
 
@@ -16,12 +16,11 @@ def parse_arguments() -> argparse.Namespace:
     return args
 
 
-
 def main():
     args = parse_arguments()
     frame_width, frame_height = args.webcam_resolution
     
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
@@ -30,8 +29,8 @@ def main():
     corner_detection_model = YOLO("models/corner_detection_best.pt")
     
     while True:
-        # ret, frame = cap.read()
-        frame = cv2.imread("board.jpg")
+        ret, frame = cap.read()
+        # frame = cv2.imread("board.jpg")
         
         detections = piece_detections(
             model=piece_detection_model, 
@@ -49,13 +48,14 @@ def main():
             board = draw_board()
             xy = generate_board(board, detections, keypoints, show=False)
         
+            chessboard = Chessboard(board_pieces_xy=xy, detections=detections)
+            chessboard.rotate_anticlockwise()
+            
+            final = chessboard.chessboard()
+        else:
+            final = draw_blank()
+            
         cv2.imshow("frame", frame)
-        
-        chessboard = Chessboard(board_pieces_xy=xy, detections=detections)
-        chessboard.rotate_anticlockwise()
-        
-        final = chessboard.chessboard()
-
         cv2.imshow("chessboard", final)
 
         if cv2.waitKey(20) == 27:
